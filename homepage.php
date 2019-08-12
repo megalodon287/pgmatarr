a1fd729095e4f662813e19d696879cae7929a58a
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-12 18:01:15 +0800

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Photo Gallery</title>
+</head>
+<style>
+
+div.c {
+  line-height:.02px;
+}
+
+</style>
+
+<body style="background-color:	#FFCC00;">
+<br>
+<br>
+
+<div class="c">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:	#FA8072;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:white;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#20B2AA;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+</div>
+<br>
+
+<h1 style="text-align:center; font-family: Arial;font-size:33px;color:black;"><i>l&nbsp&nbspi&nbsp&nbsps&nbsp&nbspt&nbsp&nbsp&nbsp&nbspo&nbsp&nbspf&nbsp&nbsp&nbsp&nbspp&nbsp&nbsph&nbsp&nbspo&nbsp&nbspt&nbsp&nbspo&nbsp&nbsps</i></h1>
+
+
+<p style="text-align:center; font-family: Arial;font-size:20px;color:black;">
+<i><a href="file:///C:/Users/regin/Downloads/photo1.html">p&nbsph&nbspo&nbspt&nbspo&nbsp&nbspo&nbspn&nbspe</a></i></p>
+<p style="text-align:center; font-family: Arial;font-size:20px;color:black;">
+<i><a href="file:///C:/Users/regin/Downloads/photo2.html">p&nbsph&nbspo&nbspt&nbspo&nbsp&nbspt&nbspw&nbspo</a></i></p>
+<p style="text-align:center; font-family: Arial;font-size:20px;color:black;"><i><a href="file:///C:/Users/regin/Downloads/photo3.html">p&nbsph&nbspo&nbspt&nbspo&nbsp&nbspt&nbsph&nbspr&nbspe&nbspe</a></i></p>
+
+
+
+
+</body>
+</html>
