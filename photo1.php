0cc35525393f09cc71d28852127ff4083324eec3
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-12 18:10:57 +0800

    Create photo1.php

---
+++ photo1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Photo Gallery</title>
+</head>
+<style>
+
+div.c {
+  line-height:.02px;
+}
+
+</style>
+
+<body style="background-color:	#FFCC00;">
+
+<div class="c">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:	#FA8072;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#A9A9A9;">
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:white;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+<h1 style="text-align:center; font-family: Arial;font-size:41px;color:#20B2AA;"><i>m&nbspa&nbspt&nbspa&nbspr&nbsp&nbsp&nbspp&nbsph&nbspo&nbspt&nbspo&nbspg&nbspa&nbspl&nbspl&nbspe&nbspr&nbspy</i></h1>
+</div>
+
+<br>
+<br>
+
+<center><img src="https://i.pinimg.com/originals/63/61/77/636177a3fafc115d67809d7ab0c47d86.gif" alt="W3Schools.com"style="width:500px;height:500px;"></center>
+
+<br>
+<br>
+
+<p style="text-align:left; font-family: Arial;font-size:20px;color:black;"><left><a href="file:///C:/Users/regin/Downloads/homepage.html"><i>back to home</i></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="file:///C:/Users/regin/Downloads/photo2.html"><i>next photo</i></a></p>
+
+</body>
+</html>
+
+
