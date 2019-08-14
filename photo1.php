684712d40806fa9adafdde422bfe64c83c2e139d
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-14 19:57:47 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -30,7 +30,7 @@ div.c {
 <br>
 
 <p style="text-align:center; font-family: Arial;font-size:20px;color:black;"><a href="index.php"><i>back to home || </a><a href="photo2.php"><i>next photo</i></a></p>
-  <center><a href="login.html"><i>l&nbspo&nbspg&nbsp&nbsp&nbspo&nbspu&nbspt</i></a></center>
+ <p style="text-align:center; font-family: Arial;font-size:20px;color:black;"><a href="login.html"><i>l&nbspo&nbspg&nbsp&nbsp&nbspo&nbspu&nbspt</i></a></p>
 
 
 </body>
