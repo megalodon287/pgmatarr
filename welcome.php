f4350160daca7ecab69461eee84f8545d2454c29
megalodon287 <53499794+megalodon287@users.noreply.github.com>
2019-08-15 22:34:45 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -11,18 +11,21 @@ div.c {
 
 </style>
 
-<body style="background-color:	#FFCC00;">
-<br>
-<br>
+<body style="background-color:#FFCC00;">
+<br/>
+<br/>
 
 <div class="c">
 <h1 style="text-align:center; font-family: Arial;font-size:60px;color:	#FA8072;"><i>w&nbsp&nbspe&nbsp&nbspl&nbsp&nbspc&nbsp&nbspo&nbsp&nbspm&nbsp&nbspe</i></h1>
 <h1 style="text-align:center; font-family: Arial;font-size:60px;color:white;"><i>w&nbsp&nbspe&nbsp&nbspl&nbsp&nbspc&nbsp&nbspo&nbsp&nbspm&nbsp&nbspe</i></h1>
 <h1 style="text-align:center; font-family: Arial;font-size:60px;color:#20B2AA;"><i>w&nbsp&nbspe&nbsp&nbspl&nbsp&nbspc&nbsp&nbspo&nbsp&nbspm&nbsp&nbspe</i></h1>
 </div>
-<br>
+<br/>
 
-<h1 style="text-align:center; font-family: Arial;font-size:23px;color:black;"><a href="https://pgmatarr.herokuapp.com/"><i>proceed to photogallery</i></h1>
+<h3 style="text-align:center; font-family: Arial;font-size:18px;color:black;"><a href="https://pgmatarr.herokuapp.com/"><i>proceed to photogallery</i></h3>
+<br/>
+<br/>
+<br/>
 
 
 <center><img src="https://i.pinimg.com/originals/39/94/16/39941694c059227504077342713c77f2.gif" style="width:650px;height:650px;"></center>
